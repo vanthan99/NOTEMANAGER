2d9a8bb6c86450610abd09f08dce812009298ed4
vanthan99 <vanthan.ad.it@gmail.com>
2020-07-29 18:41:51 +0700

    upload code

---
+++ precache-manifest.61c4cccb5150368b20858664f3b2a217.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f4b2dada4a3f67b530eafc5f77629ba",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5d327c64e8bfe82eab5a",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "4cc1490f33743d94b2c0",
+    "url": "/static/js/2.3a962177.chunk.js"
+  },
+  {
+    "revision": "0e29a10bd364030e9e7d595c87364d6c",
+    "url": "/static/js/2.3a962177.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5d327c64e8bfe82eab5a",
+    "url": "/static/js/main.e4b09730.chunk.js"
+  },
+  {
+    "revision": "b7c3487370abb6df1085",
+    "url": "/static/js/runtime-main.cf506b5b.js"
+  }
+]);
